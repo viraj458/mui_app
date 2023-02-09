c4da9695ec0edb5e3d9a91394837b0730bb192f5
Viraj <virajmalaka94@gmail.com>
2023-02-09 11:50:00 +0530

    initial config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {React} from 'react';
+import Feed from './components/Feed';
+import Sidebar from './components/Sidebar';
+import Rightbar from './components/Rightbar';
+import { Box } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Sidebar/>
+      <Feed/>
+      <Rightbar/>
+    </Box>
   );
 }
 
